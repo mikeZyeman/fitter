0e17e2cd2507d6e08e2e4661d554ca3644752e3d
miZye <mi.zyeman@gmail.com>
2019-04-08 23:33:18 +0200

    Added TestFile for mocha test and travis.yml for testing and deployment

--- dist/app/components/Implementer.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Implementer {
-}
-exports.Implementer = Implementer;
--- dist/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-const application_1 = require("./app/application");
-const index_cli_1 = require("./index-cli");
-module.exports = { Application: application_1.Application, CLI: index_cli_1.CLI };
